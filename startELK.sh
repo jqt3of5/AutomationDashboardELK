4661fba403a728958e0b4a474c99c7e9d7e7ff53
John Todd <jqtodd@verisk.com>
2020-04-27 17:15:46 -0600

    Removed old files.

--- startELK.sh
+++
@@ -1,1 +0,0 @@
-docker run -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.2
--- startLogstash.sh
+++
@@ -1,1 +0,0 @@
-docker run --net=elastic --name logstash --volume=logstash.yml:/usr/share/logstash/config/logstash.yml docker.elastic.co/logstash/logstash:7.6.2
